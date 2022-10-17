d103da7c4950d05094cb365d87d6ae83bba7dc20
ruipsmartins <rui_pedru@hotmail.com>
2022-10-17 20:04:07 +0100

    first input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
-import './app.css';
+import React, { Component } from "react";
+import "./app.css";
+
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      task: { text: "" },
+      tasks: [],
+    };
   }
-  state = {  }
-  render() { 
-    return <h1>hello world</h1>
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: "" },
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    );
   }
 }
- 
+
 export default App;
