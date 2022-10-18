87027f29867f4c1a94296d0ef3b4b58dee1eccfa
ruipsmartins <rui_pedru@hotmail.com>
2022-10-18 11:36:53 +0100

    mapping state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 import "./app.css";
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      task: { text: "" },
+      task: {
+        text: "",
+        id: uniqid(),
+      },
       tasks: [],
     };
   }
@@ -14,25 +20,29 @@ class App extends Component {
     this.setState({
       task: {
         text: e.target.value,
+        id: this.state.task.id,
       },
     });
   };
-
 
   onSubmitTask = (e) => {
     e.preventDefault();
     this.setState({
       tasks: this.state.tasks.concat(this.state.task),
-      task: { text: "" },
+      task: {
+        text: "",
+        id: uniqid(),
+      },
     });
   };
 
   render() {
     const { task, tasks } = this.state;
+
     return (
       <div>
         <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task: </label>
+          <label htmlFor="taskInput">Enter task</label>
           <input
             onChange={this.handleChange}
             value={task.text}
@@ -41,6 +51,7 @@ class App extends Component {
           />
           <button type="submit">Add Task</button>
         </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
